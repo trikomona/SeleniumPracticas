3be058e4bec0d087179afbdd9160e1ba854ec7d8
Tania Cardona <tania.cardona.cardenas@gmail.com>
2025-05-21 19:09:55 -0600

    21 de mayo

--- src/test/java/InteractWithElements.java
+++ src/test/java/InteractWithElements.java
@@ -2,6 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class InteractWithElements {
 
@@ -40,7 +44,7 @@ public class InteractWithElements {
         WebElement enterName = driver.findElement(By.xpath("//input[@name=\"enter-name\"]"));
         enterName.sendKeys("Tania");  //nota, tambien se le pueden enviar variables previamente declaradas
 
-        Thread.sleep(4000); //con fines academicos
+        Thread.sleep(2000); //con fines academicos
 
         // borrar los text box
         countryTxtbox.clear(); //borrar lo que se escribio previamente
@@ -48,84 +52,157 @@ public class InteractWithElements {
 
         //comandos GET
         //getTitle()
-        System.out.println(driver.getTitle());  //traer el titulo en consola
-        System.out.println(driver.getCurrentUrl());//traer el url de la página
+//        System.out.println(driver.getTitle());  //traer el titulo en consola
+//        System.out.println(driver.getCurrentUrl());//traer el url de la página
+//
+//        //traernos el texto del siguient elemento
+//
+//        //encontrat el elemento
+//        WebElement radioBtn = driver.findElement(By.xpath("//legend[text()='Radio Button Example']"));
+//        WebElement suggestionBtn = driver.findElement(By.xpath("//legend[text()='Suggession Class Example']"));
+//
+//
+//        //imprimir en consola
+//
+//        System.out.println(radioBtn.getText());
+//        System.out.println(suggestionBtn.getText());
+//
+//        //comandos condicionaes
+//        WebElement hideshowTxtbx = driver.findElement(By.xpath("//input[@id='displayed-text']"));
+//        System.out.println("El estatus del textbox isDisplayes es " + hideshowTxtbx.isDisplayed());
+//        showBtn.click();
+//        System.out.println("ahora el estatus del textbox isDisplayed es " + hideshowTxtbx.isDisplayed());
+//        Thread.sleep(2000);
+//
+//        //buscar radio button
+//        WebElement radioBtn1 = driver.findElement(By.xpath("//input[@value='radio1']"));
+//
+//        //Verificar si esta seleccionad e imprimir el status
+//        System.out.println("El estatus del radio botton 1 isDisplayes es " + radioBtn1.isSelected()); //false
+//
+//        //dar clic en el raddio button
+//        radioBtn1.click();
+//        System.out.println("ahora el estatus del radio botton 1 isDisplayed es " + radioBtn1.isSelected()); //true
+//
+//            Thread.sleep(2000);
+//
+//        //buscar check box 2
+//        WebElement checkBox = driver.findElement(By.xpath("//input[@id='checkBoxOption2']"));
+//
+//        //Verificar si esta seleccionad e imprimir el status
+//        System.out.println("El estatus del checkbox 2 isDisplayed es " + checkBox.isSelected()); //false
+//
+//        //clck para checkbox
+//        checkBox.click();
+//        System.out.println("ahora el estatus del checkbox 2 isDisplayed es " + checkBox.isSelected()); //true
+//
+//        //seleccionar el siguiente botóin y validar que este este deshabilitado
+//
+//
+//
+//        //encontrar elemento
+//        WebElement radioBtn2 = driver.findElement(By.xpath("//input[@value='radio2']"));
+//
+//        //verificar si esta seleccionado e imprimir status
+//        System.out.println("El estatus del radiobutton 2 isDisplayes es " + radioBtn2.isSelected()); //false
+//
+//        //dar clic en el raddio button 2
+//        radioBtn2.click();
+//        System.out.println("ahora el estatus del radio botton 2 isDisplayed es " + radioBtn2.isSelected()); //true
+//
+//        //validar en estado del radio button 1 en conjunto al seleccionar el 2
+//
+//        System.out.println("ahora el estatus del radio botton 1 isDisplayed es " + radioBtn1.isSelected()); //false
+//
+//        //dar click en el check box 3 y validar que esten seleccionados checkbox 2 y 3
+//
+//        //encontrat elemento
+//        WebElement checkBox3 = driver.findElement(By.xpath("//input[@id='checkBoxOption3']"));
+//
+//        //verificar si esta seleccionado e imprimir status
+//        System.out.println("El estatus del checkbox3 isDisplayes es " + checkBox3.isSelected()); //false
+//
+//
+//        //dar clic en el checkBox 3
+//        checkBox3.click();
+//        System.out.println("ahora el estatus del checkBox3 isDisplayed es " + checkBox3.isSelected()); //true
+//
+//        //validar en estado del checkBox2 y que esten seleccionados 2 y 3
+//
+//        System.out.println("ahora el estatus del checkBox 2 isDisplayed es " + checkBox.isSelected()); //true
+//
 
-        //traernos el texto del siguient elemento
+        //Manejo de alertas
 
-        //encontrat el elemento
-        WebElement radioBtn = driver.findElement(By.xpath("//legend[text()='Radio Button Example']"));
-        WebElement suggestionBtn = driver.findElement(By.xpath("//legend[text()='Suggession Class Example']"));
+        //encontrar el boton de alerta
+        WebElement btnAlerta =driver.findElement(By.xpath("//input[@id='alertbtn']"));
 
+        //Dar clic al boton de alerta
+        btnAlerta.click();
+        driver.switchTo().alert().accept(); //Dar clic para quitar la alerta
 
-        //imprimir en consola
-
-        System.out.println(radioBtn.getText());
-        System.out.println(suggestionBtn.getText());
-
-        //comandos condicionaes
-        WebElement hideshowTxtbx = driver.findElement(By.xpath("//input[@id='displayed-text']"));
-        System.out.println("El estatus del textbox isDisplayes es " + hideshowTxtbx.isDisplayed());
-        showBtn.click();
-        System.out.println("ahora el estatus del textbox isDisplayed es " + hideshowTxtbx.isDisplayed());
         Thread.sleep(2000);
 
-        //buscar radio button
-        WebElement radioBtn1 = driver.findElement(By.xpath("//input[@value='radio1']"));
 
-        //Verificar si esta seleccionad e imprimir el status
-        System.out.println("El estatus del radio botton 1 isDisplayes es " + radioBtn1.isSelected()); //false
+        //buscar el boton confirm
+        WebElement btnConfirm =driver.findElement(By.xpath("//input[@id='confirmbtn']"));
 
-        //dar clic en el raddio button
-        radioBtn1.click();
-        System.out.println("ahora el estatus del radio botton 1 isDisplayed es " + radioBtn1.isSelected()); //true
+        //click boton confirm
+        btnConfirm.click();
 
-            Thread.sleep(2000);
+        //Aceptar alerta
+        driver.switchTo().alert().accept(); //Dar clic para quitar la alerta
 
-        //buscar check box 2
-        WebElement checkBox = driver.findElement(By.xpath("//input[@id='checkBoxOption2']"));
+        //volver a hacer clic en confirm
+        btnConfirm.click();
 
-        //Verificar si esta seleccionad e imprimir el status
-        System.out.println("El estatus del checkbox 2 isDisplayed es " + checkBox.isSelected()); //false
+        //Declinar alerta
+        driver.switchTo().alert().dismiss(); //Dar clic para quitar la alerta
 
-        //clck para checkbox
-        checkBox.click();
-        System.out.println("ahora el estatus del checkbox 2 isDisplayed es " + checkBox.isSelected()); //true
+        //Manejo de dropdown
 
-        //seleccionar el siguiente botóin y validar que este este deshabilitado
+//        //buscar el elemento de dropdown
+//        WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+//
+//        //hacer una instancia de select
+//        Select select = new Select(dropdown);
+//
+//        //seleccionmos el valor que queremos. hay 3 formas:
+//        //por index
+//        select.selectByIndex(0);
+//        Thread.sleep(2000);  //para ver
+//        //por valor
+//        select.selectByValue("option3");
+//        Thread.sleep(2000);  //para ver
+//        //por texto visible
+//        select.selectByVisibleText("Option2");
 
-        //encontrar elemento
-        WebElement radioBtn2 = driver.findElement(By.xpath("//input[@value='radio2']"));
+//        Thread.sleep(2000);  //para ver
 
-        //verificar si esta seleccionado e imprimir status
-        System.out.println("El estatus del radiobutton 2 isDisplayes es " + radioBtn2.isSelected()); //false
+//        //uso de la clase accion mouse
+//        WebElement mousehoverBtn = driver.findElement(By.id("mousehover"));
+//        //hacer instancia
+//        Actions actions = new Actions(driver);
+//        //primero se muevan al elemento "iFrame Example"
+//        WebElement iframeLbl = driver.findElement(By.xpath("//legend[text()='iFrame Example']"));
+//        actions.moveToElement(iframeLbl).perform();
+//        // hacer el hover
+//        actions.moveToElement(mousehoverBtn).contextClick(mousehoverBtn).build().perform();
+//        //actions.contextClick(mousehoverBtn).perform();
+//        Thread.sleep(5000);
 
-        //dar clic en el raddio button 2
-        radioBtn2.click();
-        System.out.println("ahora el estatus del radio botton 2 isDisplayed es " + radioBtn2.isSelected()); //true
+        //uso del findElements
 
-        //validar en estado del radio button 1 en conjunto al seleccionar el 2
+        List<WebElement> encabezados = driver.findElements(By.xpath("//table[@name='courses']//th"));
+        //for para recorrer cada uno de los encabezados
+        for (int i=0; i<encabezados.size(); i++){
+            System.out.println(encabezados.get(i).getText());
+        }//for
 
-        System.out.println("ahora el estatus del radio botton 1 isDisplayed es " + radioBtn1.isSelected()); //false
-
-        //dar click en el check box 3 y validar que esten seleccionados checkbox 2 y 3
-
-        //encontrat elemento
-        WebElement checkBox3 = driver.findElement(By.xpath("//input[@id='checkBoxOption3']"));
-
-        //verificar si esta seleccionado e imprimir status
-        System.out.println("El estatus del checkbox3 isDisplayes es " + checkBox3.isSelected()); //false
-
-
-        //dar clic en el checkBox 3
-        checkBox3.click();
-        System.out.println("ahora el estatus del checkBox3 isDisplayed es " + checkBox3.isSelected()); //true
-
-        //validar en estado del checkBox2 y que esten seleccionados 2 y 3
-
-        System.out.println("ahora el estatus del checkBox 2 isDisplayed es " + checkBox.isSelected()); //true
-
-
+        //foreach  este for trae el mismo resultado
+        for (WebElement encabezado:encabezados){
+            System.out.println(encabezado.getText());
+        }//foreach
 
 
 
@@ -134,5 +211,6 @@ public class InteractWithElements {
         driver.quit();  // va a cerrar todas las ventanas abiertas
 
 
-    }//metodo main
+    }//metodo main{
+
 }//clase InteractWithElements
